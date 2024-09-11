9465abf8d75739f795d50c31e203d86a7e6b540f
JacoboLujanAlbertoE <126634586+HackoboL@users.noreply.github.com>
2024-09-11 13:19:19 -0600

    primera carga

---
+++ Scripts/main.js
@@ -0,0 +1,38 @@
+let miTitulo = document.querySelector("h1");
+miTitulo.textContent= "¡teacher!";
+
+let miImage = document.querySelector("img");
+miImage.onclick = function () {
+  let miSrc = miImage.getAttribute("src");
+  if (miSrc === "Images/Anabantha.jpg") {
+    miImage.setAttribute("src", "Images/HadaDeBeng.jpg");
+  } else {
+    miImage.setAttribute("src", "Images/Morante.jpg");
+  }
+};
+let miBoton = document.querySelector("button");
+let miTitulo = document.querySelector("h1");
+
+function estableceNombreUsuario() {
+  let miNombre = prompt("Por favor, ingresa tu nombre.");
+  localStorage.setItem("nombre", miNombre);
+  miTitulo.textContent = "Mozilla es genial," + miNombre;
+}
+if (!localStorage.getItem("nombre")) {
+  estableceNombreUsuario();
+} else {
+  let nombreAlmacenado = localStorage.getItem("nombre");
+  miTitulo.textContent = "Mozilla es genial," + nombreAlmacenado;
+};
+miBoton.onclick = function () {
+  estableceNombreUsuario();
+};
+function estableceNombreUsuario() {
+  let miNombre = prompt("Introduzca su nombre.");
+  if (!miNombre) {
+    estableceNombreUsuario();
+  } else {
+    localStorage.setItem("nombre", miNombre);
+    miTitulo.innerHTML = "Mozilla is genial, " + miNombre;
+  }
+}
